16c9806f67ee39eeef451282849c87d555ae75b4
qiguang0916 <qiguang0916@126.com>
2019-10-23 16:08:44 +0800

    测试修改1018分支

--- case/startAppium.py
+++ case/startAppium.py
@@ -18,8 +18,7 @@ def getDriver():
     }
     driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", capabilities)
     return driver
-    print('上传1018分支')
-    print('修改1018分支')
+    print('这是1018分支')
 
 driver = getDriver()
 
